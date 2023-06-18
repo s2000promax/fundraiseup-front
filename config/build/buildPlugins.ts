7235d8b7ae4e698486baa12be5ac803179e1ff18
s2000promax <s2000promax@gmail.com>
2023-06-18 15:42:30 +0400

    fix: production mode

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -23,7 +23,7 @@ export function buildPlugins(
 
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin());
-    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: true }));
   }
 
   return plugins;
